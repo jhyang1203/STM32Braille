4d3e89b0311646ce8171b128bf024ee83aa33849
JIseolyun <tjfdbs02@gmail.com>
2025-07-18 14:34:44 +0900

    Update Motor.c

--- driver/Motor.c
+++ driver/Motor.c
@@ -16,34 +16,6 @@ uint32_t motorTimChannel;
 // 3 4
 // 5 6
 
-static const uint8_t brailleMap[26][6] = {
-		{1,0,0,0,0,0}, // A
-		{1,0,1,0,0,0}, // B
-		{1,1,0,0,0,0}, // C
-		{1,1,0,1,0,0}, // D
-		{1,0,0,1,0,0}, // E
-		{1,1,1,0,0,0}, // F
-		{1,1,1,1,0,0}, // G
-		{1,0,1,1,0,0}, // H
-		{0,1,1,0,0,0}, // I
-		{0,1,1,1,0,0}, // J
-		{1,0,0,0,1,0}, // K
-		{1,0,1,0,1,0}, // L
-		{1,1,0,0,1,0}, // M
-		{1,1,0,1,1,0}, // N
-		{1,0,0,1,1,0}, // O
-		{1,1,1,0,1,0}, // P
-		{1,1,1,1,1,0}, // Q
-		{1,0,1,1,1,0}, // R
-		{0,1,1,0,1,0}, // S
-		{0,1,1,1,1,0}, // T
-		{1,0,0,0,1,1}, // U
-		{1,0,1,0,1,1}, // V
-		{0,1,1,1,0,1}, // W
-		{1,1,0,0,1,1}, // X
-		{1,1,0,1,1,1}, // Y
-		{1,0,0,1,1,1}  // Z
-};
 
 static TIM_HandleTypeDef* motorTim[6] = {
 		&htim5, &htim3, &htim5, &htim3, &htim3, &htim3
@@ -67,8 +39,6 @@ void Motor_SetAngle(uint32_t degree)
 	float pulse_ms = 1.0f + ((float)degree / 90.0f);  // 0도=1.0ms, 90도=2.0ms, 180도=3.0ms
 	uint32_t ccr = (uint32_t)(pulse_ms * 100.0f);
 
-	//	__HAL_TIM_SET_AUTORELOAD(&htim5, arr);
-	//	__HAL_TIM_SET_COMPARE(&htim5, TIM_CHANNEL_1, ccr);
 	__HAL_TIM_SET_AUTORELOAD(hmotorTim, arr);
 	__HAL_TIM_SET_COMPARE(hmotorTim, motorTimChannel, ccr);
 }
@@ -88,30 +58,23 @@ void Motor_DisplayBits(uint8_t bits6)
 
 	for (uint8_t dot = 0; dot < 6; dot++) {
 		Motor_Init(motorTim[dot], motorCh[dot]);
-		if (bits6 & (1 << (5 - dot))) {
+		if (bits6 & (1 << dot)) {
 			Motor_SetAngle(90);
 		} else {
 			Motor_SetAngle(0);
 		}
 		Motor_Start();
+		__HAL_TIM_SET_COUNTER(&htim2, 0);
+		HAL_TIM_Base_Start_IT(&htim2);
 	}
 
 }
 
-void Motor_DisplayBraille(char data)
+void Motor_ResetAllDots()
 {
-	if (data < 'A' || data > 'Z') return;
-	uint8_t idx = data - 'A';
-
 	for (uint8_t dot = 0; dot < 6; dot++) {
 		Motor_Init(motorTim[dot], motorCh[dot]);
-
-		if (brailleMap[idx][dot]) {
-			Motor_SetAngle(90);
-		}
-		else {
-			Motor_SetAngle(0);
-		}
-		Motor_Start();
+		Motor_SetAngle(0);
 	}
+	HAL_TIM_Base_Stop_IT(&htim2);  // 1회용이면 정지
 }
